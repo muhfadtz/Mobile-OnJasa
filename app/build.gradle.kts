4b5608bdebe7e7685c9d88ab67f2dba8f556d75e
muhfadtz <mhfadtz@gmail.com>
2024-10-08 12:21:43 +0700

    Added GetStartedActivity to user First Login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
